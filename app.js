10c0ccf11fe9e5826acfd0b39183aa617d585a6b
krp312 <krp312@me.com>
2017-08-15 21:45:05 -0400

    accordion animation working

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-      <script>
-        $('.nav-section a').on('click', function(event) {
-          event.preventDefault();
-          $('.nav-section-inner').remove('open-inner');
-          $(this).sibling('.nav-section').addClass('open-inner')
-        });
-      </script>
+let activeSection;
+$('.nav-section a').on('click', function(event) {
+  event.preventDefault();
+  activeSection.find('.nav-section-inner').remove('open-inner');
+  $(this).sibling('.nav-section-inner').toggleClass('open-inner')
+  activeSection = $(this);
+
+  // add minus class, remove plus class
+});
