8b21064feb600e97df629e03fe98a11cb422fad5
krp312 <krp312@me.com>
2017-08-16 08:41:16 -0400

    attempt to fix expand/collapse

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 $(function() {
   $('.nav-section a').on('click', function(event) {
     event.preventDefault();
-    // $(this).parent().css('background-color', 'black');
     $(this).parents('.container').find('.nav-section-inner').removeClass('open-inner');
     $(this).siblings('.nav-section-inner').toggleClass('open-inner');
+
+    // if else??
   });
 });
