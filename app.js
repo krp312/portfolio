77f5342792a03c1e41420c3f388375fef8f6eb1c
krp312 <krp312@me.com>
2017-08-23 16:12:19 -0400

    install accordion plugin

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
+// $(function() {
+//   $('.nav-section a').on('click', function(event) {
+//     event.preventDefault();
+
+//     $(this)
+//       .siblings('.nav-section-inner')
+//       .toggleClass('open-inner');
+
+//     $('.open-inner')  // div.nav-section-inner.open-inner
+//       .not($(this).siblings()) // don't select contact.nav-section-inner
+//       .removeClass('open-inner');
+
+//     // $(this)
+//     //   .find('h2')
+//     //   .toggleClass('plus minus');
+//   });
+// });
+
+var defaultConfig = {
+  headersSelector: '.js-accordion__header',
+  panelsSelector: '.js-accordion__panel',
+  buttonsSelector: 'button.js-accordion__header',
+  buttonsGeneratedContent: 'text', // html
+  button: $('', {
+    class: 'js-accordion__header',
+    type: 'button'
+  }),
+  buttonSuffixId: '_tab',
+  multiselectable: true,
+  prefixClass: 'accordion',
+  headerSuffixClass: '__title',
+  buttonSuffixClass: '__header',
+  panelSuffixClass: '__panel',
+  direction: 'ltr', // rtl
+  accordionPrefixId: 'accordion'
+};
+
 $(function() {
-  $('.nav-section a').on('click', function(event) {
-    event.preventDefault();
-  
-    $(this)
-      .siblings('.nav-section-inner')
-      .toggleClass('open-inner');
-
-    $('.open-inner')  // div.nav-section-inner.open-inner
-      .not($(this).siblings()) // don't select contact.nav-section-inner
-      .removeClass('open-inner');
-
-    // $(this)
-    //   .find('h2')
-    //   .toggleClass('plus minus');
-  });
+  $('.js-accordion').accordion(defaultConfig);
 });
