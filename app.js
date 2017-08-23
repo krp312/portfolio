dd4433949aa0cdeae558d5b3374c6770a6276c9d
krp312 <krp312@me.com>
2017-08-23 17:29:47 -0400

    individually color buttons

--- app.js
+++ app.js
@@ -16,25 +16,25 @@
 //   });
 // });
 
-var defaultConfig = {
-  headersSelector: '.js-accordion__header',
-  panelsSelector: '.js-accordion__panel',
-  buttonsSelector: 'button.js-accordion__header',
-  buttonsGeneratedContent: 'text', // html
-  button: $('', {
-    class: 'js-accordion__header',
-    type: 'button'
-  }),
-  buttonSuffixId: '_tab',
-  multiselectable: true,
-  prefixClass: 'accordion',
-  headerSuffixClass: '__title',
-  buttonSuffixClass: '__header',
-  panelSuffixClass: '__panel',
-  direction: 'ltr', // rtl
-  accordionPrefixId: 'accordion'
-};
-
 $(function() {
+  const defaultConfig = {
+    headersSelector: '.js-accordion__header',
+    panelsSelector: '.js-accordion__panel',
+    buttonsSelector: 'button.js-accordion__header',
+    buttonsGeneratedContent: 'text', // html
+    // button: $('', {
+    //   class: 'js-accordion__header',
+    //   type: 'button'
+    // }),
+    buttonSuffixId: '_tab',
+    multiselectable: false,
+    prefixClass: 'accordion',
+    headerSuffixClass: '__title',
+    buttonSuffixClass: '__header',
+    panelSuffixClass: '__panel',
+    direction: 'ltr', // rtl
+    accordionPrefixId: 'accordion'
+  };
+
   $('.js-accordion').accordion(defaultConfig);
 });
