0dc43aad096f1ffb761ec35f5862b45fd20b0574
krp312 <krp312@me.com>
2017-08-23 22:20:02 -0400

    inside panel cleanup, font sizing

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
-// $(function() {
-//   $('.nav-section a').on('click', function(event) {
-//     event.preventDefault();
-
-//     $(this)
-//       .siblings('.nav-section-inner')
-//       .toggleClass('open-inner');
-
-//     $('.open-inner')  // div.nav-section-inner.open-inner
-//       .not($(this).siblings()) // don't select contact.nav-section-inner
-//       .removeClass('open-inner');
-
-//     // $(this)
-//     //   .find('h2')
-//     //   .toggleClass('plus minus');
-//   });
-// });
-
 $(function() {
   const defaultConfig = {
     headersSelector: '.js-accordion__header',
