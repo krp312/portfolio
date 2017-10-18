23c424582d2ee84af8e1ad8d3de3e797ff7aa4f0
krp312 <krp312@me.com>
2017-10-18 11:41:39 -0400

    Add npm start script

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 $(function() {
-  const defaultConfig = {
-    headersSelector: '.js-accordion__header',
-    panelsSelector: '.js-accordion__panel',
-    buttonsSelector: 'button.js-accordion__header',
-    buttonsGeneratedContent: 'text', // html
-    // button: $('', {
-    //   class: 'js-accordion__header',
-    //   type: 'button'
-    // }),
-    buttonSuffixId: '_tab',
-    multiselectable: false,
-    prefixClass: 'accordion',
-    headerSuffixClass: '__title',
-    buttonSuffixClass: '__header',
-    panelSuffixClass: '__panel',
-    direction: 'ltr', // rtl
-    accordionPrefixId: 'accordion'
-  };
+    const defaultConfig = {
+        headersSelector: '.js-accordion__header',
+        panelsSelector: '.js-accordion__panel',
+        buttonsSelector: 'button.js-accordion__header',
+        buttonsGeneratedContent: 'text', // html
+        // button: $('', {
+        //   class: 'js-accordion__header',
+        //   type: 'button'
+        // }),
+        buttonSuffixId: '_tab',
+        multiselectable: false,
+        prefixClass: 'accordion',
+        headerSuffixClass: '__title',
+        buttonSuffixClass: '__header',
+        panelSuffixClass: '__panel',
+        direction: 'ltr', // rtl
+        accordionPrefixId: 'accordion'
+    };
 
-  $('.js-accordion').accordion(defaultConfig);
+    $('.js-accordion').accordion(defaultConfig);
 });
