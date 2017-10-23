16b6e3cc25d963b079b35c6f0b6d7134304dba8c
krp312 <krp312@me.com>
2017-10-23 06:50:04 -0400

    Contact form sends field data to server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public/images'));
@@ -8,6 +13,10 @@ app.use(express.static(__dirname + '/public/styles'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/contact', (req, res) => {
+    console.log('here', req.body);
 });
 
 app.listen(process.env.PORT || 8080);
